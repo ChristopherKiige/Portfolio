7de450c7f46b573cf6d8431e14d1e2b29ab4ad1c
ChristopherKiige <christopher.kiige@ontariotechu.net>
2025-09-16 21:44:06 -0400

    Fix the read more button in the about section

--- script.js
+++ script.js
@@ -44,4 +44,20 @@ function typeEffect() {
     }
 }
 
+function readFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("my-btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
 typeEffect();
